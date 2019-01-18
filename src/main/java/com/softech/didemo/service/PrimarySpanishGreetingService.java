520cfaaf0bf5a895cf171cbf70e159d29b6356e3
Huynh <nguyenviethuynh1996.vih@gmail.com>
2019-01-18 16:50:39 +0700

    primary default

--- src/main/java/com/softech/didemo/service/PrimarySpanishGreetingService.java
+++ src/main/java/com/softech/didemo/service/PrimarySpanishGreetingService.java
@@ -7,11 +7,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
-
+@Profile("es")
+public class PrimarySpanishGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service";
+        return "Primary Spanish Greeting Service";
     }
 }
